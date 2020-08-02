c38ce4faacd56271c7419bce190b01ae434074f7
Felipe-Alafy-Rodrigues-Silva <68610472+Felipe-Alafy@users.noreply.github.com>
2020-08-02 12:15:22 -0300

    Update Ex023.py

--- desafios/Ex023.py
+++ desafios/Ex023.py
@@ -1,6 +1,9 @@
-num = str(input("Digite um número entre 0 e 9999: "))
-valor = num[0:4]
-print("unidade: {}".format(valor[3]))
-print("dezena:  {}".format(valor[2]))
-print("centena: {}".format(valor[1]))
-print("milhar:  {}".format(valor[0]))
+num = int(input("Digite um número entre 0 e 9999: "))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print("unidade: {}".format(u))
+print("dezena:  {}".format(d))
+print("centena: {}".format(c))
+print("milhar:  {}".format(m))
