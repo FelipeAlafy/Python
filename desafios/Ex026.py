93f70e5e4b81c0b2b7e7b673c078bf3ba90ef39b
Felipe-Alafy-Rodrigues-Silva <68610472+Felipe-Alafy@users.noreply.github.com>
2020-08-02 12:26:39 -0300

    Update Ex026.py

--- desafios/Ex026.py
+++ desafios/Ex026.py
@@ -1,7 +1,7 @@
 frase = str(input("Digite uma frase qualquer: ")).strip().upper()
 A = frase.count('A')
-Ap = frase.find('A')
-Aup = frase.rfind('A')
+Ap = frase.find('A')+1
+Aup = frase.rfind('A')+1
 print("""A letra \"A\" aparece {} vezes na frase.
 A primeira posição em que ela aparece é em {}.
 A última posição em que ela aparece é em {}.""".format(A, Ap, Aup))
