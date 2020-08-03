5b119c32dade8e64f354537f626e26149c276c71
Felipe-Alafy-Rodrigues-Silva <68610472+Felipe-Alafy@users.noreply.github.com>
2020-08-03 12:02:35 -0300

    Update Ex032.py

--- desafios/Ex032.py
+++ desafios/Ex032.py
@@ -5,7 +5,7 @@ ano = int(input("Digite um ano qualquer, ou 0 para o ano atual: "))
 if ano == 0:
     ano = anoAtual
 
-if ano % 4 == 0 and ano % 100 != 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print("O ano {}, é BISSEXTO!".format(ano))
 else:
     print("O ano {}, NÃO é BISSEXTO!".format(ano))
