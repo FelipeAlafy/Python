d29835712cb008eba0d258707fca78b849b7bf0c
Felipe-Alafy-Rodrigues-Silva <68610472+Felipe-Alafy@users.noreply.github.com>
2020-08-03 11:03:47 -0300

    Ex034
    Um programa que pergunta ao usuário o salário atual dele e verifica, quanto será a porcentagem de  aumento do salário, e depois efetua a conta e mostra  o novo salário com aumento.

---
+++ desafios/Ex034.py
@@ -0,0 +1,7 @@
+sal = float(input("Qual é o seu salário atual? "))
+if sal >= 1250:
+    aume = 10
+else:
+    aume = 15
+nsal = sal + ((sal * aume)/100)
+print("O seu salário era {:.2f}, mas com um aumento de {}% ele passou a ser {:.2f}.".format(sal, aume, nsal))
