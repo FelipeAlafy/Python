826e91cecb1006484843fdb0d6fc998dc725f8f0
Felipe-Alafy-Rodrigues-Silva <68610472+Felipe-Alafy@users.noreply.github.com>
2020-08-03 11:59:48 -0300

    Ex032 - update
    agora pode pegar o ano atual do sistema e calcular se é bissexto ou não.

--- desafios/Ex032.py
+++ desafios/Ex032.py
@@ -1,4 +1,10 @@
-ano = int(input("Digite um ano qualquer: "))
+from datetime import datetime
+now = datetime.now()
+anoAtual = now.year
+ano = int(input("Digite um ano qualquer, ou 0 para o ano atual: "))
+if ano == 0:
+    ano = anoAtual
+
 if ano % 4 == 0 and ano % 100 != 0:
     print("O ano {}, é BISSEXTO!".format(ano))
 else:
